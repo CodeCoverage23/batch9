11122568b6fffc96df1810768b3c082d7d61685d
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-07-30 12:17:58 +0530

    default value and print number

---
+++ Demo/src/com/pallavi/basics/DefaultNumber.java
@@ -0,0 +1,35 @@
+package com.pallavi.basics;
+
+public class DefaultNumber {
+
+	
+		static int i;
+		   static byte b;
+		   static boolean t;
+		   static short s;
+		   static long l;
+		   static double d;
+		   static float f;
+		   
+		    
+
+			public static void main(String[] args) {
+				System.out.println("Default Values");
+				System.out.println("i=" +i);
+				System.out.println("b=" +b);
+				System.out.println("t=" +t);
+				System.out.println("s=" +s);
+				System.out.println("l=" +l);
+				System.out.println("d=" +d);
+				System.out.println("f=" +f);
+				
+				
+
+			}
+
+		
+
+
+	}
+
+
---
+++ Demo/src/com/pallavi/basics/PrintNumber.java
@@ -0,0 +1,60 @@
+package com.pallavi.basics;
+
+public class PrintNumber {
+
+	public static void main(String[] args) {
+		 
+		int i;
+		
+		for(i=1;i<=100;i++)
+			
+			
+			 
+		 
+		
+			
+		{
+			
+			
+			int x=5;
+			  int y=7;
+			  int FullName=x*y;
+			  System.out.println(FullName);
+			
+	 if(i%5<=0)
+		
+	 {
+		 System.out.println("First Name:Pallavi");
+	 }
+	 else
+		 
+		if (i%7<=0)
+		{
+			System.out.println("Last Name:Ghodake");
+		}
+		else
+			if(i%5>=0 && i%7>=0)
+			{
+				System.out.println("Full Name: Pallavi Ghodake");
+				
+			}	
+	 /*int x=5;
+	  int y=7;
+	  int FullName=x*y;
+	  System.out.println(FullName);
+	  */
+	 System.out.println(i);
+	 
+	 
+		}
+	 
+	 
+	 
+	 
+	 
+		
+		
+	
+
+	}
+}
