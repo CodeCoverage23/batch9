c1c66ef09a60f3119e0783abe3ef497ce71b0af2
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-07-30 12:25:08 +0530

    again print number 100

---
+++ Demo/src/com/pallavi/basics/PrintNumberJava.java
@@ -0,0 +1,13 @@
+package com.pallavi.basics;
+
+public class PrintNumberJava {
+
+	public static void main(String[] args) {
+		
+		int i;
+		for(i=1; i<=100; i++)
+			
+			System.out.println(i);
+	}
+
+}
