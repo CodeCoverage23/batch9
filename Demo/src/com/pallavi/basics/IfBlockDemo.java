8180db731307776a80161dc88a8f353e101090aa
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-07-30 12:21:03 +0530

    if block demo

---
+++ Demo/src/com/pallavi/basics/IfBlockDemo.java
@@ -0,0 +1,16 @@
+package com.pallavi.basics;
+
+public class IfBlockDemo {
+
+	public static void main(String[] args) {
+       
+		int age =18;
+		
+		
+		if (age>=10) 
+			System.out.println("Can Apply For Pan");
+			
+
+	}
+
+}
