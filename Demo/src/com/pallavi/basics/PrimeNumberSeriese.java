4116d7086dfb1808a7f95f03751eadaf8a20fdae
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-02 21:59:39 +0530

    Prime number series

---
+++ Demo/src/com/pallavi/basics/PrimeNumberSeriese.java
@@ -0,0 +1,40 @@
+package com.pallavi.basics;
+
+import java.util.Scanner;
+
+public class PrimeNumberSeriese {
+
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		
+		System.out.println("Please enter the number to print the seriese of prime number");
+		int number=sc.nextInt();
+		if(number>1)
+		{
+			for( int i=2;i<=number;i++)
+			{
+				int count=0;
+				for(int j=1;j<=i;j++) {
+					if(i%j==0) {
+						count++;
+						
+					}
+						
+				}
+				if(count==2) {
+					
+				System.out.println(i+" ");
+				}
+			}
+		}
+			else
+			{
+		    System.out.println("Please enter valid number to print prime number seriese ");
+				
+			}	
+		
+	
+	}
+
+}
