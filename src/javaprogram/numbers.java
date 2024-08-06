ece988b05b70bade0192c37e613893028b8bcb45
pooja.hande <pooja.hande@ITLNB1136.kiya.ai>
2024-08-06 14:19:25 +0530

    during class programs

---
+++ src/javaprogram/numbers.java
@@ -0,0 +1,13 @@
+package javaprogram;
+
+public class numbers {
+ 
+	public static void main(String args[]) {
+			int i ;	
+		for(i=1;i<=100;i++) 
+		{
+			System.out.println("The next number is : " +i );
+			
+		}
+	}
+}
