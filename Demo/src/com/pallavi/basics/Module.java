0da25be2f0edfb876133153cb849b68ac0ebbfbc
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-06 12:54:09 +0530

    using module

---
+++ Demo/src/com/pallavi/basics/Module.java
@@ -0,0 +1,19 @@
+package com.pallavi.basics;
+
+public class Module {
+
+	public static void main(String[] args) {
+		int items = 23;
+		int result = items % 5;
+		System.out.println(result);
+		
+		
+		int x = 8;
+		int y = 3;
+		int result1 = x % y;
+		System.out.println(result1);
+		// TODO Auto-generated method stub
+
+	}
+
+}
