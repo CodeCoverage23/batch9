7294837244578ebb78c8cb7349a00268a153a380
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-06 18:26:55 +0530

    operator

--- Demo/src/com/pallavi/basics/Operator.java
+++ Demo/src/com/pallavi/basics/Operator.java
@@ -6,11 +6,13 @@ public class Operator {
 		
 		int num = 7;
 		num++; // post increment
+		       //fetch the value and then increment
 		
 		System.out.println(num);
 		
 		int num1 = 7;
 		++num; // pre-increment 
+		       // increment and then fetch the value
 		System.out.println(num1);
 		
 	
