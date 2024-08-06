cd79d636dfa0f1ae5eda826526210da13cf274b4
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-06 18:23:50 +0530

    post increment and pre-increment

---
+++ Demo/src/com/pallavi/basics/Operator.java
@@ -0,0 +1,19 @@
+package com.pallavi.basics;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		
+		int num = 7;
+		num++; // post increment
+		
+		System.out.println(num);
+		
+		int num1 = 7;
+		++num; // pre-increment 
+		System.out.println(num1);
+		
+	
+   }
+
+  }
