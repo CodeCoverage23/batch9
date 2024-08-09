73defe8b6f5000cbb9908361b6e6c08fef6efe2e
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-09 13:05:31 +0530

    Basic operator

---
+++ Demo/src/com/pallavi/basics/Basics.java
@@ -0,0 +1,14 @@
+package com.pallavi.basics;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String country = "US";
+		int age = 42;
+		if((country=="US" || country=="GB")&&(age>0 && age<100))
+		{
+			System.out.println("Allowed");
+		}
+}
+}
