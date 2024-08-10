0aae72d1cb11edb1842023252664d6c4a43d4c0a
Dell <Dell@192.168.1.36>
2024-08-10 12:32:54 +0530

    Defaultconstructor

---
+++ JavaProgram/src/com/basic/constructor/DefaultConstructor.java
@@ -0,0 +1,13 @@
+package com.basic.constructor;
+
+public class DefaultConstructor {
+
+	public DefaultConstructor() {
+		System.out.println("I am default constructor");
+	}
+
+	public static void main(String[] args) {
+		DefaultConstructor defaultContructor = new DefaultConstructor();
+	}
+
+}
