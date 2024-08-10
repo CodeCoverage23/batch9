7a3f1661f599ce078ccd60d3e70a69c10a4e66dd
Dell <Dell@192.168.1.36>
2024-08-10 12:32:29 +0530

    DataTypes

---
+++ JavaProgram/src/com/basic/DataTypes.java
@@ -0,0 +1,37 @@
+package com.basic;
+
+public class DataTypes {
+
+	byte b;
+	short s;
+	int i;
+	long l;
+	float f;
+	static double d;
+	boolean b1;
+	char c;
+
+	public static void main(String[] args) {
+
+		float e = 11.2f;
+		System.out.println(e);
+
+		double g = 120.20d;
+
+		char ch = 'S';
+		System.out.println(ch);
+
+		boolean t = true;
+		System.out.println(t);
+
+		System.out.println(d);
+
+		int a = 100;
+		++a;
+		System.out.println(a);
+		--a; // 99 or 100
+		System.out.println(a);
+
+	}
+
+}
