bfe2287307253ceeb5e2a40ecd4340abc1d22cef
Dell <Dell@192.168.1.36>
2024-08-10 12:02:16 +0530

    SwitchProgram

---
+++ JavaProgram/src/com/java/program/SwitchProgram.java
@@ -0,0 +1,62 @@
+
+	package com.java.program;
+
+	public class SwitchProgram {
+
+		public static void main(String[] args) {
+	          
+			int month = 1 ;
+			
+			String monthString= "";
+			
+			switch(month) {
+			
+			case 1 : monthString="january";
+			System.out.println("the month for the expresion is : "+monthString);
+			break;
+			case 2 : monthString="february";
+			System.out.println("the month for the expresion is : "+monthString);
+
+			break;
+			
+			case 3 : monthString="march";
+			System.out.println("the month for the expresion is : "+monthString);
+
+			break;
+			
+			case 4 : monthString="april";
+			System.out.println("the month for the expresion is : "+monthString);
+
+			break;
+			
+			case 5 : monthString="may";
+			System.out.println("the month for the expresion is : "+monthString);
+
+			break;
+			
+			case 6 : monthString="june";
+			System.out.println("the month for the expresion is : "+monthString);
+
+			break;
+			
+			case 7 : monthString="july";
+			System.out.println("the month for the expresion is : "+monthString);
+
+			break;
+			
+			case 8 : monthString="august";
+			System.out.println("the month for the expresion is : "+monthString);
+
+			break;
+			
+			default : System.out.println("invalid input");
+			break;
+			}
+			
+			
+			
+		}
+
+	}
+
+
