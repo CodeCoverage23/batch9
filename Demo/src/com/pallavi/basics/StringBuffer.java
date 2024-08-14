b45af778443088b56f7c9228b37613cc462606ed
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-14 23:31:54 +0530

    StringBufferBsics

---
+++ Demo/src/com/pallavi/basics/StringBuffer.java
@@ -0,0 +1,15 @@
+package com.pallavi.basics;
+
+public class StringBuffer {
+
+	public StringBuffer(String string) {
+		
+	}
+	public static void main(String[] args) {
+		
+		StringBuffer sb =  new StringBuffer("Pallavi");
+		System.out.println(sb.hashCode());
+		
+	}
+
+}
---
+++ Demo/src/com/pallavi/basics/StringBufferBasic.java
@@ -0,0 +1,13 @@
+package com.pallavi.basics;
+
+public class StringBufferBasic {
+
+	public static void main(String[] args) {
+		
+		StringBuffer sb = new StringBuffer(null);
+		System.out.println(sb.equals(null));
+		
+
+	}
+
+}
