3684cfd07f628d497eccd4fd20b8b6fa5dbf4678
Dell <Dell@192.168.1.36>
2024-08-15 21:20:05 +0530

    ReverseString

---
+++ JavaProgram/src/com/java/program/ReverseString.java
@@ -0,0 +1,28 @@
+package com.java.program;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a string>>");
+		String str = sc.next();
+		String rev="";
+		int length = str.length();	
+		System.out.println("string before reverse>>");
+		System.out.println(str);
+		
+		StringBuffer bf= new StringBuffer(str);
+		StringBuffer reverse = bf.reverse();
+		
+		System.out.println("string After reverse>>");
+		System.out.println(reverse);
+		
+		
+		
+		
+	}
+
+}
