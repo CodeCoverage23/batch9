62e4bb9fd71c702bac681b5c2aa277a08b00c3a9
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-15 21:18:44 +0530

    FrequencyOfString with some comments

--- Demo/src/com/pallavi/basics/FrequencyString.java
+++ Demo/src/com/pallavi/basics/FrequencyString.java
@@ -4,18 +4,20 @@ package com.pallavi.basics;
 public class FrequencyString {
 
 	public static void main(String[] args) {
-		 String str = "Pallavi is awesome.";
-	       char ch = 'p'; 
-	        int frequency = 0;
+		//given string
+		 String str = "Pallavis coding is awesome.";
+		 
+	       char ch = 'i'; //character whose frequency must be determined
+	        int frequency = 0; //store the frequency in frequency
 
-	        for(int i = 0; i < str.length(); i++) {
-	        	if(ch==str.charAt(i))
+	        for(int i = 0; i < str.length(); i++) { //Iterate through the string
+	        	if(ch==str.charAt(i))  //if the specified character appear in the string
 	          {
-	                ++frequency;
+	                ++frequency; //increase frequency by 1
 	            }
 	        }
 
-	        System.out.println("Frequency of "+ ch  + " = " + frequency);
+	        System.out.println("Frequency of "+ ch  + " = " + frequency);  //print the total frequency of 'char'
 	    }
 
 	}
