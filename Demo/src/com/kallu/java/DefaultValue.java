f1d1e54a7b70ea842cc645680b8edd5eb65b8b5e
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-15 16:40:00 +0530

    Default Value

---
+++ Demo/src/com/kallu/java/DefaultValue.java
@@ -0,0 +1,26 @@
+package com.kallu.java;
+
+public class DefaultValue {
+	static int i;
+	   static byte b;
+	   static boolean t;
+	   static short s;
+	   static long l;
+	   static double d;
+	   static float f;
+
+
+
+		public static void main(String[] args) {
+			System.out.println("Default Values");
+			System.out.println("i=" +i);
+			System.out.println("b=" +b);
+			System.out.println("t=" +t);
+			System.out.println("s=" +s);
+			System.out.println("l=" +l);
+			System.out.println("d=" +d);
+			System.out.println("f=" +f);
+
+		}
+
+}
