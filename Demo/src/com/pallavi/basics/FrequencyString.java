0d6a179307f11bb800d338e3902c97d828c421a9
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-15 21:08:29 +0530

    FrequencyOfString

---
+++ Demo/src/com/pallavi/basics/FrequencyString.java
@@ -0,0 +1,23 @@
+package com.pallavi.basics;
+
+
+public class FrequencyString {
+
+	public static void main(String[] args) {
+		 String str = "Pallavi is awesome.";
+	       char ch = 'p'; 
+	        int frequency = 0;
+
+	        for(int i = 0; i < str.length(); i++) {
+	        	if(ch==str.charAt(i))
+	          {
+	                ++frequency;
+	            }
+	        }
+
+	        System.out.println("Frequency of "+ ch  + " = " + frequency);
+	    }
+
+	}
+
+
