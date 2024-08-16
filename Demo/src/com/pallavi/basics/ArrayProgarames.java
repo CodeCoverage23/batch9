4b04411378997fb2b06caf29f05b9e25dc6f6285
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-16 14:18:25 +0530

    ArrayProgrameWithBasics

---
+++ Demo/src/com/pallavi/basics/ArrayProgarames.java
@@ -0,0 +1,17 @@
+package com.pallavi.basics;
+
+public class ArrayProgarames {
+
+	public static void main(String[] args) {
+	
+		int[] nums = {3, 8, 5, 2};
+		int res =0;
+		for(int x: nums) {
+			if(x> res) {
+				res = x;
+			}
+		}
+		System.out.println(res);
+	}
+
+}
