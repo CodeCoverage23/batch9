047af4cdde7e3144a548991fa35ef4a14b11c5f0
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-17 14:32:44 +0530

    Even Odd Number using For loop

---
+++ Demo/src/com/kallu/java/EvenOddNumbersDemo.java
@@ -0,0 +1,20 @@
+package com.kallu.java;
+
+public class EvenOddNumbersDemo {
+	 
+  
+	public static void main(String[] args) {
+		
+
+		 for(int i=1;i<=1000;i++) {
+			 if(i%2==0) {
+				 System.out.println("Even Number");
+			 }else {
+				 System.out.println("Odd Number");
+				 
+			 }
+			 System.out.println(i);
+			 }
+		}
+		}
+	
