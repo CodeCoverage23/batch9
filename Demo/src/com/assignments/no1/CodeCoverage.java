0bc88dc8eba47d0d0f163ca88fc25b187108ef0c
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-08-23 23:38:09 +0530

    assignment 1

---
+++ Demo/src/com/assignments/no1/CodeCoverage.java
@@ -0,0 +1,10 @@
+package com.assignments.no1;
+
+public class CodeCoverage {
+	public static void main(String[]args) {
+		//print the code coverage message to console
+		System.out.println("code coverage:Follow the decision ");
+		
+	}
+
+}
