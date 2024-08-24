a3a19dbb3ca6b8d0e56e5195d9d8549bc5b6f3fa
Dell <Dell@192.168.1.34>
2024-08-24 14:30:05 +0530

    Fristcharacter

---
+++ JavaProgram/src/com/basic/Assignment2/FristCharacter.java
@@ -0,0 +1,13 @@
+package com.basic.Assignment2;
+
+public class FristCharacter {
+	public static void main(String[] args) {
+	
+	String name = "Kalyani";
+
+	char firstCharacter = name.charAt(0);
+	
+	System.out.println("The frist character of your name: "  + firstCharacter);
+	}
+
+}
