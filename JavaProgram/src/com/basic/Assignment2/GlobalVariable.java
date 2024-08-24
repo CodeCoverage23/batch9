23afd9bd3c6002a38c89d86eef4c0edb39014f8c
Dell <Dell@192.168.1.34>
2024-08-24 14:30:35 +0530

    globalvariable

---
+++ JavaProgram/src/com/basic/Assignment2/GlobalVariable.java
@@ -0,0 +1,16 @@
+package com.basic.Assignment2;
+
+public class GlobalVariable {
+	
+	static int y = 90;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("The value of y is: "+y);
+		
+		y = 50;
+		
+		System.out.println("The value of y is: "+y);
+		
+	}
+}
