6fc4a20860be7261ad8b96fc8be45b0b45f7ee39
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-26 18:19:44 +0530

    Assignment 2.4

---
+++ Demo/src/com/assignments/basics/Assignment2four.java
@@ -0,0 +1,18 @@
+package com.assignments.basics;
+
+public class Assignment2four {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int x =50; //Assign the value 
+		System.out.println(x);
+		
+		
+		
+		
+		
+	}
+
+}
