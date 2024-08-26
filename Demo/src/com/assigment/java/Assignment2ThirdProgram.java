450d974ef09d2e4174cb487dabb9cf184bfe2969
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-26 17:44:54 +0530

    Declare the local variable as x & local variable value as 50

---
+++ Demo/src/com/assigment/java/Assignment2ThirdProgram.java
@@ -0,0 +1,13 @@
+package com.assigment.java;
+
+public class Assignment2ThirdProgram {
+
+	public static void main(String[] args) {
+		// declare the local variable as x & local variable value as 50
+
+		int X =50; // Local variable 
+		
+		System.out.println("X = "+X);
+	}
+
+}
