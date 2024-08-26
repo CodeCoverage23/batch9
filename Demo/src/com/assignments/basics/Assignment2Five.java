85d739e54e8abea03c744ef3c64385b7677d35ad
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-26 18:20:29 +0530

    Assignment 2.5

---
+++ Demo/src/com/assignments/basics/Assignment2Five.java
@@ -0,0 +1,14 @@
+package com.assignments.basics;
+
+public class Assignment2Five {
+	
+	public static int y = 25; //declare globle variable and print the value
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Global Variable y: " + y);
+		
+		
+		
+	}
+}
