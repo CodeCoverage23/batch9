27125c136caa48f19600a89e38fa1d0dbf00e37b
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-26 17:04:52 +0530

    Code Coverage print on Console

---
+++ Demo/src/com/assigment/java/Assignment1.java
@@ -0,0 +1,12 @@
+package com.assigment.java;
+
+public class Assignment1 {
+
+	//WAP to print code coverage Message
+	
+	public static void main (String arg[]) {
+		
+		System.out.println("Code Coverage");
+	
+	}
+}
