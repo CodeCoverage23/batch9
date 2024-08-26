206218cdc20fb67ca427a4dc83cf7cd11e1bde8f
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-08-26 18:16:38 +0530

    Assignment 2.2

---
+++ Demo/src/com/assignments/basics/Assignment2Two.java
@@ -0,0 +1,26 @@
+package com.assignments.basics;
+
+public class Assignment2Two {
+
+	public static void main(String[] args) {
+		
+		
+		String name = "Pallavi";
+		char firstCharacter = name.charAt(0);
+		System.out.println("The first character of the name is: " +firstCharacter);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
