6d755bf3d149c8fcfad82718e6ad7f56e659316e
pooja.hande <pooja.hande@ITLNB1136.kiya.ai>
2024-08-27 01:16:49 +0530

    Assignment 3

---
+++ src/com/assignment/Assignment3.java
@@ -0,0 +1,18 @@
+package com.assignment;
+
+public class Assignment3 {
+
+	public String getStudentName(String sname) {
+		
+		return sname;
+	}
+	public static void main(String args[]) {
+		
+		Assignment3 a3 = new Assignment3();
+		
+		
+		System.out.println("Studnt Name : " + (a3.getStudentName("Pooja")));
+		
+		
+	}
+}
