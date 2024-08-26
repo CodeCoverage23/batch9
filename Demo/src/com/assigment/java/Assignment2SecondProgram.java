9f2fee874f9703ccdaa6e5c7b60538d070a666fc
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-26 17:32:42 +0530

    print first character of your name on console.

---
+++ Demo/src/com/assigment/java/Assignment2SecondProgram.java
@@ -0,0 +1,19 @@
+package com.assigment.java;
+
+public class Assignment2SecondProgram {
+		
+		 public static void firstCharacter(String str)
+		    {
+			 int a = str.length();
+			 char first = str.charAt(0);
+
+		System.out.println("First Character of Name is: " + first);
+		}
+		public static void main(String args[])
+		    {
+			String str = "Kalyani";
+			firstCharacter(str);
+		
+	
+}
+}
