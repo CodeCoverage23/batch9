a2c6f0480c16ea24ab566e23821a527d66af9478
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-08-28 01:00:35 +0530

    print the global variable value as 25

---
+++ Demo/src/com/assignments/no2/GlobalVariableValue.java
@@ -0,0 +1,12 @@
+package com.assignments.no2;
+
+public class GlobalVariableValue {
+	
+	static int y = 25;
+	
+	public static void main(String[]arg) {
+		
+		System.out.println("print the value of y:"+y);
+	}
+
+}
