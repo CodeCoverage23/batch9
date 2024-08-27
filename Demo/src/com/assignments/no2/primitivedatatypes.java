bce42eee24d765430c32c1f4f4386732eaa3176b
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-08-28 00:04:36 +0530

    assignment no 2 primitive data types

---
+++ Demo/src/com/assignments/no2/primitivedatatypes.java
@@ -0,0 +1,37 @@
+package com.assignments.no2;
+
+public class primitivedatatypes {
+   
+public static void main(String[] args) {
+	
+    //integer type
+	byte myByte = 100;          
+    short myShort = 20000;       
+    int myInt = 400000000;      
+    long myLong = 500000000000L; 
+    
+    // Floating point types
+    float myFloat = 4.5f;       
+    double myDouble = 17.77d;    
+    
+    // Character type
+    char myChar = 'c';          
+    
+    // Boolean type
+    boolean myBoolean = false;    
+    
+    //print the values
+    System.out.println("Byte: " + myByte);
+    System.out.println("Short: " + myShort);
+    System.out.println("Int: " + myInt);
+    System.out.println("Long: " + myLong);
+    System.out.println("Float: " + myFloat);
+    System.out.println("Double: " + myDouble);
+    System.out.println("Char: " + myChar);
+    System.out.println("Boolean: " + myBoolean);   
+       
+    
+}
+
+
+}
