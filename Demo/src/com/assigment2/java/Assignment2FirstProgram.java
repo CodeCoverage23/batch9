a02504be942c78c505416a3995998489c2137230
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-27 09:36:39 +0530

    declares the different primitives data type

---
+++ Demo/src/com/assigment2/java/Assignment2FirstProgram.java
@@ -0,0 +1,45 @@
+package com.assigment.java;
+
+public class Assignment2FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 //Boolean 
+
+		boolean var1 = true;
+		      if (var1 == true) //checks if the value is true or false
+		      {
+		          System.out.println("Boolean value is True");
+		      }
+		      else
+		      {
+		          System.out.println("Boolean value is False");  
+		      }
+
+		//Character
+		      
+		char v1 = 'A';
+		        char v2 = 'd';
+		        System.out.println(v1);
+		        System.out.println(v2);
+
+		// Int,Short,byte,long
+		int a = 10;
+		    short s = 2;
+		    byte b = 6;
+		    long l = 125362133223l;
+		 
+		    System.out.println("The integer variable is " + a + '\n');
+		    System.out.println("The short variable is " + s  + '\n');
+		    System.out.println("The byte variable is " + b + '\n');
+		    System.out.println("The long variable is " + l);
+
+		//Float
+		float f = 65.20298f;
+		    double d = 876.765d;
+		 
+		    System.out.println("The float variable is " + f);
+		    System.out.println("The double variable is " + d);
+	}
+
+}
