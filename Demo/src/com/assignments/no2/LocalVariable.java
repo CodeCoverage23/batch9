62119d2a440a7c2291d96678fc3ff7aceb8c67d4
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-08-28 00:30:43 +0530

    local variable example x

---
+++ Demo/src/com/assignments/no2/LocalVariable.java
@@ -0,0 +1,17 @@
+package com.assignments.no2;
+
+public class LocalVariable {
+ 
+	public static void main(String[] args) {
+	        
+		// Declare a local variable x
+	       int x = 40;  
+
+	    // Print the value of x
+	        System.out.println("The value of x is: " + x);
+	   
+	}
+	
+}
+
+
