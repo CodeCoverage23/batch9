93137dd5de116683f5c5aa2ed7e2c98d7c880042
Hp <Hp@DESKTOP-S3AF537>
2024-08-27 12:48:16 +0530

    Hello Java

---
+++ Demo/src/com/nisarg/java/HelloJava.java
@@ -0,0 +1,11 @@
+package com.nisarg.java;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Java");
+	}
+
+}
