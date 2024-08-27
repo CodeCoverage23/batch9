84ec48ff9d1706040f17e034a98b7e75a08bca7e
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-08-28 00:44:13 +0530

    global variable

---
+++ Demo/src/com/assignments/no2/GlobalVariable.java
@@ -0,0 +1,12 @@
+package com.assignments.no2;
+
+public class GlobalVariable {
+	
+	static int y = 200;
+	
+	public static void main(String[]arg) {
+		
+		System.out.println("print the value of y:"+y);
+	}
+
+}
