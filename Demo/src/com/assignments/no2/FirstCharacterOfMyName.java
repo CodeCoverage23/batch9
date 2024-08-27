ef2a658e0d2a41fcef2763046b188f7ef55cf5c6
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-08-28 00:19:06 +0530

    print the first character of my name to console

---
+++ Demo/src/com/assignments/no2/FirstCharacterOfMyName.java
@@ -0,0 +1,18 @@
+package com.assignments.no2;
+
+public class FirstCharacterOfMyName {
+
+	public static void main(String[] args) {
+       
+		// Declare a string with my name
+        String myName = "Priyanka"; 
+
+        //  first character of the string
+        char firstChar = myName.charAt(0);
+
+        // Print the first character to the console
+        System.out.println("The first character of my name is: " + firstChar);
+    }
+}
+
+
