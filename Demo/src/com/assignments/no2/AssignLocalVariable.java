4ec2e0dd8821e744f17c662387ebae4f7b5dd3ef
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-08-28 00:38:28 +0530

    Assign local variable as 50

---
+++ Demo/src/com/assignments/no2/AssignLocalVariable.java
@@ -0,0 +1,12 @@
+package com.assignments.no2;
+
+public class AssignLocalVariable {
+	public static void main(String[]args) {
+		
+		int x = 50; //declare a value and then assign as 50
+		
+		//print the value
+		System.out.println("print the value of:" +x);
+	}
+
+}
