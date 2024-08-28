be2455f41bf2865096a6932ec89ec175b0ae01c1
Hp <Hp@DESKTOP-S3AF537>
2024-08-28 17:39:30 +0530

    Assignment3

---
+++ Demo/src/com/asssignmet_3/basic/Assignment3.java
@@ -0,0 +1,26 @@
+package com.asssignmet_3.basic;
+
+public class Assignment3 {
+	// Method to get student name
+	public String getStudentName(String name) {
+
+		// Print student name
+		System.out.println("Student Name: " + name);
+		// Return the student name
+		return name;
+	}
+
+	public static void main(String[] args) {
+
+		// Create an instance of student class
+
+		Assignment3 student = new Assignment3();
+
+		// Call the getStudentName method and store the result
+
+		String studentName = student.getStudentName("Nisarg Meshram");
+
+		System.out.println("Return student name: " + studentName);
+
+	}
+}
