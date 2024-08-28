441ad892c65f2ff052c2ee228ce5fd3eb13eb4bc
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-28 18:18:24 +0530

    Separate method as String getStudentName (String name) which return student name

---
+++ Demo/src/com/assignment3/java/assignment3.java
@@ -0,0 +1,18 @@
+package com.assignment3.java;
+
+public class assignment3 {
+
+public String getStudentName(String sname) {
+		
+		return sname;
+	}
+	public static void main(String args[]) {
+		
+		assignment3 a3 = new assignment3();
+		
+		
+		System.out.println("Studnt Name : " + (a3.getStudentName("Kalyani Devidas Kashid")));
+		
+		
+	}
+}
