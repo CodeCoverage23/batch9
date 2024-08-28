c944bf36869e52fbcaf1dc9f8c4060bf15ca8ba6
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-28 18:36:00 +0530

    design method for division of two number which returns int value to that method

---
+++ Demo/src/com/assignment4/java/Assignment4_4.java
@@ -0,0 +1,20 @@
+package com.assignment4.java;
+
+public class Assignment4_4 {
+	
+	//design method for division of two number which returns int value to that method 
+	
+	public int div(int a,int b) {
+		int c= a/b;
+		return c;
+	}
+	public static void main(String[] args) {
+		
+		Assignment4_4 a10 = new Assignment4_4();
+		
+		
+		System.out.println("Division - Answer is : " +a10.div(5243, 2));
+
+	}
+
+}
