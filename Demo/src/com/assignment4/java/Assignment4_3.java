146eb21919ae5d5b5be4337313c75af3361028ac
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-28 19:37:16 +0530

    design method for multiplication of two number which returns int value to that method

---
+++ Demo/src/com/assignment4/java/Assignment4_3.java
@@ -0,0 +1,23 @@
+package com.assignment4.java;
+
+public class Assignment4_3 {
+	
+	//design method for multiplication of two number which returns int value to that method
+	
+	public int multi(int a,int b) {
+		int c= a*b;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		
+		
+		Assignment4_3 a4 = new Assignment4_3();
+		
+		
+		System.out.println("Multiplication - Answer is :" +a4.multi(45, 78));
+		
+
+	}
+
+}
