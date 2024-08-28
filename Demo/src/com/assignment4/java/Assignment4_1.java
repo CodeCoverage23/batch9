e1487894231f319620ce3878f82c34b800bcd057
Kalyani <Kalyani@LAPTOP-I848S1M4>
2024-08-28 18:24:58 +0530

    Addition of two number which returns int results to that method and result should be print into main method

---
+++ Demo/src/com/assignment4/java/Assignment4_1.java
@@ -0,0 +1,19 @@
+package com.assignment4.java;
+
+public class Assignment4_1 {
+	//addition of two number which returns int results to that method
+	
+	public int add(int a,int b) {
+		int c= a+b;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		
+		Assignment4_1 a4 = new Assignment4_1();
+		
+		
+		System.out.println("Addition :" +a4.add(700, 252));
+	}
+
+}
