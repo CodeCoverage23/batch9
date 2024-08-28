8cfb366a67b3465f120c601715f7fcd545902b00
Hp <Hp@DESKTOP-S3AF537>
2024-08-28 09:10:52 +0530

    primitive data

---
+++ Demo/src/Assignment2/Assignment2_1.java
@@ -0,0 +1,27 @@
+package Assignment2;
+
+public class Assignment2_1 {
+
+	
+public static void main(String[] args) {
+		
+		
+		int    a = 10;
+		short  b = 89;
+		double c = 70.0;
+		float  d = 56f;
+		byte   e = 89;
+		long   f = 67;
+		
+		
+		System.out.println("a:" + a);
+		System.out.println("b:" + b);
+		System.out.println("c:" + c);
+		System.out.println("d:" + d);
+		System.out.println("e:" + e);
+		System.out.println("f:" + f);
+		
+	}
+
+}
+}
