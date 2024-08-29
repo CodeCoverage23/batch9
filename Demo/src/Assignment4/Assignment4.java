7a911407f0edb429af5f11aa8430a7cb108c5255
Hp <Hp@DESKTOP-S3AF537>
2024-08-29 07:52:10 +0530

    Assignment4

---
+++ Demo/src/Assignment4/Assignment4.java
@@ -0,0 +1,15 @@
+package Assignment4;
+
+public class Assignment4 {
+
+	public int multi(int a,int b) {
+		int c= a*b;
+		return c;
+	}
+
+	public static void main(String args[]) {
+		Assignment4 a4 = new Assignment4();
+
+
+		System.out.println("Multiplication :" +a4.multi(10, 20));
+	}}
