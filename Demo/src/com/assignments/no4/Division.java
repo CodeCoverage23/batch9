fd38a2b8ccd21e8ecfea6e605abe7923b0752d1b
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-08-31 10:16:51 +0530

    Division of two number

---
+++ Demo/src/com/assignments/no4/Division.java
@@ -0,0 +1,22 @@
+package com.assignments.no4;
+
+public class Division {
+
+	    public static int divide(int num1, int num2) {
+	        
+	        return num1 / num2;
+	    }
+
+	    public static void main(String[] args) {
+	     
+	        int number1 = 90;
+	        int number2 = 4;
+
+	        int result = divide(number1, number2);
+
+	        System.out.println("The result of the division is: " + result);
+	    }
+	}
+
+
+
