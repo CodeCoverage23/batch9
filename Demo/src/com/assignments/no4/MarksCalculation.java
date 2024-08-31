ad5d62b9e4a5495203e012b40b8335fa14bd15e2
Priyanka <Priyanka@LAPTOP-BKHVGJOE>
2024-08-31 10:22:37 +0530

    Calculate the total marks

---
+++ Demo/src/com/assignments/no4/MarksCalculation.java
@@ -0,0 +1,27 @@
+package com.assignments.no4;
+
+public class MarksCalculation {
+
+ public static void main(String[] args) {
+	       
+	 //  marks for five subjects
+	        int subject1 = 10;
+	        int subject2 = 40;
+	        int subject3 = 98;
+	        int subject4 = 78;
+	        int subject5 = 22;
+
+	        // Calculate the total marks
+	        int totalMarks = subject1 + subject2 + subject3 + subject4 + subject5;
+
+	        // Calculate the average marks
+	        double averageMarks = totalMarks / 5.0;
+
+	        // Print the total and average marks
+	        System.out.println("Total marks: " + totalMarks);
+	        System.out.println("Average marks: " + averageMarks);
+	    }
+	}
+
+
+
