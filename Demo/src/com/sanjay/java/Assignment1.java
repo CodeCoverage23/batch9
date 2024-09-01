810f3978902c2d11e2f91d5ca215897b06e46dae
Admin <Admin@DESKTOP-TRG7850>
2024-09-01 12:31:18 +0530

    print code coverage msg

---
+++ Demo/src/com/sanjay/java/Assignment1.java
@@ -0,0 +1,13 @@
+package com.sanjay.java;
+
+public class Assignment1 {
+
+	//WAP to print code coverage Message
+	public static void main(String[] args) 
+	{
+		System.out.println("Code Coverage");
+
+
+	}
+
+}
