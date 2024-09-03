4161efed32c44c86a94d4157c4df6225c74cf99f
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-09-04 00:33:15 +0530

    Assignments 6

---
+++ Demo/src/com/assignments_6/java/Assignment6_3.java
@@ -0,0 +1,23 @@
+package com.assignments_6.java;
+
+// print all even number from 50 to 75.
+
+public class Assignment6_3 {
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Even number between 50 to 75: ");
+		
+		
+		for(int x = 50; x <= 75; x++) {
+		
+		if(x % 2 == 0) {
+			System.out.println(x);
+		}
+		
+		}
+		
+	}
+
+}
