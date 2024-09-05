2783752e98e81aae53380f11de2e38389ed25a57
Admin <Admin@DESKTOP-TRG7850>
2024-09-05 09:54:10 +0530

    declare the global variables as y & print the global variable value as 25.

---
+++ Demo/src/com/sanjay/java/Assignment2ForthAndFithProgram.java
@@ -0,0 +1,15 @@
+package com.sanjay.java;
+
+public class Assignment2ForthAndFithProgram {
+
+	public static void main(String[] args) {
+		// declare the global variables as y & print the global variable value as 25.
+		
+				var Y =25 ; // Global variable 
+				
+				System.out.println("Y = "+Y);
+
+
+	}
+
+}
