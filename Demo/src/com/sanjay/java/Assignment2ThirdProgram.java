2d880dfd55b0790401a559b20f23b187385103e2
Admin <Admin@DESKTOP-TRG7850>
2024-09-05 09:51:44 +0530

    declare the local variable as y & local variable value as 50

---
+++ Demo/src/com/sanjay/java/Assignment2ThirdProgram.java
@@ -0,0 +1,14 @@
+package com.sanjay.java;
+
+public class Assignment2ThirdProgram {
+
+	public static void main(String[] args) {
+		// declare the local variable as x & local variable value as 50
+
+		int y =10; // Local variable 
+		
+		System.out.println("y = "+y);
+
+	}
+
+}
