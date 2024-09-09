b5475a84f3904b4a2cc70099a8e4438399117405
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-09-09 21:50:33 +0530

    StringConcatning

---
+++ Demo/src/com/pallavi/basics/SimplePrograme.java
@@ -0,0 +1,13 @@
+package com.pallavi.basics;
+
+public class SimplePrograme {
+
+	public static void main(String[] args) {
+		String firstName = "Pallavi";
+		String lastName = " Ghodake";
+		String fullName = firstName.concat(lastName);
+		System.out.println(fullName);
+
+	}
+
+}
