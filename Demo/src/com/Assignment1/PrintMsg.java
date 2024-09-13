f510c8c70be45f4bad3e8a1717885aeb53d74e5b
pratiksha naik <pratikshanaik3008@gmail.com>
2024-09-13 23:35:47 +0530

    Assignment 1

---
+++ Demo/src/com/Assignment1/PrintMsg.java
@@ -0,0 +1,10 @@
+package com.Assignment1;
+//1. Write the Java program to print the Code Coverage message on console.
+public class PrintMsg {
+
+	public static void main(String[] args) {
+		System.out.println("Code Coverage");
+
+	}
+
+}
