77bb6858c2360a61345c80db357e7c83537f31bc
pratiksha naik <pratikshanaik3008@gmail.com>
2024-09-14 22:44:03 +0530

    Assignment 3

---
+++ Demo/src/com/Assignment3/StudentName.java
@@ -0,0 +1,15 @@
+package com.Assignment3;
+//1. Design the separate method as String getStudentName (String name) which return student name and print it.
+
+public class StudentName {
+	public static String getStudentName(String name) {
+		return name;
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println(getStudentName("Jiyaa"));
+
+	}
+
+}
