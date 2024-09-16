d3ab5c1acdbad77db21825c3b100bff7e4fd04a1
dell 5480 <dell 5480@DESKTOP-TDUQJND>
2024-09-16 21:52:13 +0530

    Assignment 1

---
+++ Demo/Ass/com/java/assignment1/PrintName.java
@@ -0,0 +1,11 @@
+package com.java.assignment1;
+
+// Write a java program to print the Code Coverage message on console.
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		System.out.println("Code Coverage");
+	}
+
+}
