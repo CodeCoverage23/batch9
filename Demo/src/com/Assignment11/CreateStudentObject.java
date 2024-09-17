037501764998e6678867f169616113406451ded3
pratiksha naik <pratikshanaik3008@gmail.com>
2024-09-17 14:07:01 +0530

    Assignment 11

---
+++ Demo/src/com/Assignment11/CreateStudentObject.java
@@ -0,0 +1,12 @@
+package com.Assignment11;
+
+public class CreateStudentObject {
+
+	public static void main(String[] args) {
+	//	Student s=new Student();			//we can not create object of class student cause it have private constructor
+	//	s.value=22;
+	//	System.out.println(s.value);
+
+	}
+
+}
---
+++ Demo/src/com/Assignment11/Student.java
@@ -0,0 +1,14 @@
+package com.Assignment11;
+
+public class Student {
+	private Student() {
+		
+	}
+	int value=20;
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
