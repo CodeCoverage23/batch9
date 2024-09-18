345f0150937de47ec4952f98960e6479a3033a3a
dell 5480 <dell 5480@DESKTOP-TDUQJND>
2024-09-18 23:24:32 +0530

    Assignment 3

---
+++ Demo/Ass/com/java/assignment3/StringStudentName.java
@@ -0,0 +1,22 @@
+package com.java.assignment3;
+
+// Design the separate method as String getStudentName (String name) which return student name
+// and print it.
+
+public class StringStudentName {
+
+	public static void main(String[] args) {
+
+		System.out.println(StringStudentName.getStudentName());
+
+	}
+
+	public static String getStudentName() {
+
+		String name = "Yogeshwari";
+
+		return name;
+
+	}
+
+}
