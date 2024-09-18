e6159cc39afd35a32c74f563c92559ffc8a33c5d
Pallavi <Pallavi@LAPTOP-2TNDS4KO>
2024-09-18 22:57:28 +0530

    Multidimensional array

---
+++ Demo/src/com/pallavi/basics/MultidimensionalArray.java
@@ -0,0 +1,11 @@
+package com.pallavi.basics;
+
+public class MultidimensionalArray {
+	public static void main(String[] args) {
+		int array [][]  = {{3, 5, 8},{7, 54, 1, 12, 4}};
+		
+		System.out.println( array[0][2]); // array[0] refer to the first row of the array
+		                                  //array[0][2] access the element 2nd index first row thats why the output is 8
+	}
+
+}
